ac1144fee29f908a3f1ff3e9b7bc0e66a241cc03
KEVIN <farpistudent1@gmail.com>
2026-09-13 15:46:11 +0900

    feat: learn Python booleans

--- primitive.py
+++ primitive.py
@@ -20,10 +20,29 @@ result = type(course)
 print(f"the result (1): {result}")
 
 result = course.title()
-print(f"the result (1): {result}")
+print(f"the result (1): {result}")      # har bir soz bosh harf b/n boshlanishi
 
 result = course.upper()
+print(f"the result (1): {result}")      # Hamma harflar katta
+
+result = course.replace("FullStack", "MasterClass")     # so'zni replace qilish
 print(f"the result (1): {result}")
 
-result = course.replace("FullStack", "MasterClass")
-print(f"the result (1): {result}")
+print("========= boolean ===========")
+# functions> type() input() bool() int() str()
+
+y = input("Give your value for y: ")
+print("y:", y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# TRUTHY vs FALSY value
+# TRUTHY: True, 100, -100, "MIT"
+# FALSY: False, 0, "", None
+
+test_falsy = "" or False or None or 0
+print("test_falsy:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("test_truthy:", bool(test_truthy))
