a80ee579e4a73c5ff7c21112003f09c854ee299b
KEVIN <farpistudent1@gmail.com>
2026-09-16 19:51:55 +0900

    feat: specific error handling system

--- object.py
+++ object.py
@@ -29,3 +29,18 @@ print("result1: ", result1)
 
 result2 = ceil(98.7)  # yaxshlitlash
 print("result2: ", result2)
+
+
+print("==== Error Handling System ====")
+car_dict = dict(name="Toyota", year=2027, electric=True)
+
+try:
+    print("Passed here")
+    result = car_dict["year"]
+    print("result: ", result)
+except KeyError as err:
+    print("No origin state property found: ", err)
+else:
+    print("Executed successfully")
+finally:
+    print("Final closing logic")
